0c9dfd7f54d20377ef7cf3343ada212464225dbc
aakim-git <Aaron Kim>
2019-10-28 10:26:24 -0700

    moved navigation.js to header. Added header.css

--- src/components/header.js
+++ src/components/header.js
@@ -1,46 +1,55 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
-import Navigation from '../components/navigation'
+import "../styles/header.css"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-      <Navigation/>
-    </div>
-
-
-  </header>
+    <header>
+        <div class = "navbar">
+        
+            <div class = "navbar-logo">
+                <a href="/">
+                    <img alt="TEMPORARY LOGO" src = "http://icons.iconarchive.com/icons/graphicloads/100-flat/256/home-icon.png"></img><br/>
+                </a>
+            </div>
+            
+            <div class = "navbar-menu">
+                <ul class = "navbar-menu-items">
+                    <li id = "navbar-menu-item-1">
+                        <a href = "google.com">
+                            ABOUT US
+                        </a>
+                    </li>
+                    
+                    <li id = "navbar-menu-item-2">
+                        <a href = "google.com">
+                            SERVICES
+                        </a>
+                    </li>
+                    
+                    <li id = "navbar-menu-item-2">
+                        <a href = "google.com">
+                            DONATE
+                        </a>
+                    </li>
+                    <li id = "navbar-menu-item-3">
+                        <a href = "google.com">
+                            CONTACT US
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            
+        </div>
+    </header>
 )
 
 Header.propTypes = {
-  siteTitle: PropTypes.string,
+    siteTitle: PropTypes.string,
 }
 
 Header.defaultProps = {
-  siteTitle: ``,
+    siteTitle: ``,
 }
 
 export default Header
