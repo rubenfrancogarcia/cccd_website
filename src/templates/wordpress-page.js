5af731c707e88381d2369e90b616a0a28e6c15a1
aakim-git <Aaron Kim>
2019-10-30 10:06:15 -0700

    Added sidebar and footer file

--- src/templates/wordpress-page.js
+++ src/templates/wordpress-page.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Layout from "../components/layout"
 import { graphql } from "gatsby"
+import "../styles/wordpress-page.css"
 
 export default ({ data }) => {
   const page = data.allWordpressPage.edges[0].node
