948ae3eda2e453ff26782353613d4d82bf4afcdd
aakim-git <Aaron Kim>
2019-11-20 13:07:45 -0800

    Began services page

--- src/pages/services.js
+++ src/pages/services.js
@@ -1,10 +1,26 @@
 import React from 'react'
 import Layout from '../components/layout'
+import "../styles/services.css"
 
 const Services = () => (
-<Layout>
-<p> schedule a meeting with us</p>
-</Layout>
+    <Layout>
+        <div id = "services-body"> 
+            <div id = "services-header">
+                <div>
+                    <h1> Our Services </h1>
+                    <p> We specialize in these things: </p>
+                </div>
+                <img src="https://live.staticflickr.com/8087/29609189022_c374c2cbd2_b.jpg"></img>
+            </div>
+            <p> 
+                asd <br/> 
+                asd <br/>
+                asd <br/>
+                asd <br/>
+            </p>
+        </div>
+    
+    </Layout>
 )
 
 export default Services
