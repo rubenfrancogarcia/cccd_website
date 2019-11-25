94248992dce3dec93dce7ca938606e7dd75f8681
aakim-git <Aaron Kim>
2019-11-25 13:27:33 -0800

    Initialized careers page files

---
+++ src/pages/careers.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/layout'
+import "../styles/careers.css"
+
+const careers = () => (
+    <Layout>
+
+    
+    </Layout>
+)
+
+export default careers
