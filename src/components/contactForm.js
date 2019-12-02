b11660961e1e67019dc482e8a78827a4a8f69778
aakim-git <Aaron Kim>
2019-12-02 09:53:47 -0800

    Reformatted contact form code

--- src/components/contactForm.js
+++ src/components/contactForm.js
@@ -22,68 +22,70 @@ export default class Contact extends React.Component {
       } 
     render() {
       return ( 
-    <div class="col-view"> 
-        <div class="box"> 
-         <div class> 
-                <h2>Drop us an Email!</h2>
-            </div>
-            <div class="container"> 
-                <form id="fs-contact" 
-                method="POST" 
-                action="https://formspree.io/moqqqygd"  
-                onSubmit={this.handleSubmit} 
-                > 
-                    <div class="item"> 
-                    <label> First Name  
-                        <input type="text" 
-                        name="firstName"
-                        value={this.state.firstName}
-                        onChange={this.handleInputChange}
-                        id="name"
-                        /> 
-                    </label>
-                    </div>
-                    <div class="item"> 
-                        <label> 
-                            Last Name 
-                            <input type="text"
-                            name="lastName"
-                            value={this.state.lastName}
-                            onChange={this.handleInputChange}
-                            id="lastName"
-                            />
-                        </label>
-                    </div>
-                    <div class="item"> 
-                    <label> Email Address 
-                        <input type="text"
-                        name="emailAddress"
-                        value={this.state.emailAddress}
-                        onChange={this.handleInputChange}
-                        id="emailAddress"
-                        />
-                    </label>
-                    </div>
-                    <div class="item"> 
-                        <label> Message
-                            <textarea rows="5"
-                                input type="text" 
-                            name="message"
-                            value={this.state.message}
-                            onChange={this.handleInputChange}
-                            id="message"
-                            required=""
-                            />
-
-                        </label>
-                    </div>
-                
-                <button type="submit" value="send">Submit</button>
-        </form>
-            </div>
-        </div>  
-    </div> 
-        
+        <div class="col-view"> 
+            <div class="box"> 
+                <div class> 
+                    <h2>Drop us an Email!</h2>
+                </div>
+                <div class="container"> 
+                    <form id="fs-contact" 
+                    method="POST" 
+                    action="https://formspree.io/moqqqygd"  
+                    onSubmit={this.handleSubmit} 
+                    > 
+                        <div class="item"> 
+                            <label> First Name  
+                                <input type="text" 
+                                name="firstName"
+                                value={this.state.firstName}
+                                onChange={this.handleInputChange}
+                                id="name"
+                                /> 
+                            </label>
+                        </div>
+                        
+                        <div class="item"> 
+                            <label> 
+                                Last Name 
+                                <input type="text"
+                                name="lastName"
+                                value={this.state.lastName}
+                                onChange={this.handleInputChange}
+                                id="lastName"
+                                />
+                            </label>
+                        </div>
+                        
+                        <div class="item"> 
+                            <label> Email Address 
+                                <input type="text"
+                                name="emailAddress"
+                                value={this.state.emailAddress}
+                                onChange={this.handleInputChange}
+                                id="emailAddress"
+                                />
+                            </label>
+                        </div>
+                        
+                        <div class="item"> 
+                            <label> Message
+                                <textarea rows="5"
+                                    input type="text" 
+                                name="message"
+                                value={this.state.message}
+                                onChange={this.handleInputChange}
+                                id="message"
+                                required=""
+                                />
+                            </label>
+                        </div>
+                    
+                        <button type="submit" value="send">Submit</button>
+                        
+                    </form>
+                </div>
+            </div>  
+        </div>         
       )
     }
   }
