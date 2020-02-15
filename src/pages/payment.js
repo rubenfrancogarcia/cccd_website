a7d57e16cc68d72f0a23c92633c69f57c2fa7974
Ruben Garcia <rubengarcia@Rubens-MacBook-Air.local>
2020-02-14 23:07:31 -0800

    added mobile friendly view of payment page

--- src/pages/payment.js
+++ src/pages/payment.js
@@ -9,9 +9,9 @@ import PaypalButtons from '../components/paypal'
 const Payment = () => (
    
     <Layout>
-        <div id = "payment-main">
+        <div id = "payment-wide-view">
             <div id = "payment-header">
-                <img src="https://p2.piqsels.com/preview/271/703/795/copy-space-notebook-wooden-journal.jpg" alt="Make a Payment - Header"></img>
+                <img src="https://p2.piqsels.com/preview/271/703/795/copy-space-notebook-wooden-journal.jpg" alt=""></img>
                 <div>
                     <h1> Online Payments </h1>
                 </div>
@@ -20,30 +20,71 @@ const Payment = () => (
             <div id = "payment-body">
                 <div id = "paypal-container">
                     <div>
-                        <h1> Make a Payment </h1>
-                        <p> If you've used our services in the past, you can make a payment here! A 2% fee will be charged for all online/credit card payments <br/> </p><br/>
+                        <h2> Make a Payment </h2>
+                        <p> If you've used our services in the past, you can make a payment here! A 2% fee will be charged for all online/credit card payments<br/> </p><br/>
                         <PaypalButtons />
                     </div>
                 </div>
                 
                 <div id = "donate-container">
                     <div>
-                        <h1> Make a Donation </h1>
+                        <h2> Make a Donation </h2>
                         <p> A tax-deductible donation to the non-profit, the <i> California Center for Cooperative Development </i> helps them support the sustainability 
                             and growth of Co-op Homecare!</p>
-                            <div class ="link-container"><a href="https://cccd.coop/membership/donate-cccd"> Click to donate to CCCD  </a> </div>
+                            <div className ="link-container"><a href="https://cccd.coop/membership/donate-cccd"> Click to donate to CCCD  </a> </div>
                     </div>
                     
                 </div>
 
                 <div id = "check-container">
                     <div>
-                        <h1> Pay by Check  </h1>
+                        <h2> Pay by Check  </h2>
                         <p> Payments via checks will not be charged a fee. Please send all checks  
                             addressed to <strong> Co-op Home Care </strong>to the following address 
                             <strong> 979 F street, Suite A-2, Davis, California 95616.</strong>.  
                         </p>
-                        <div class ="link-container">
+                        <div className ="link-container">
+                            <a href="https://goo.gl/maps/ogjx6M2Ku7BCKEUA7"> Click for Instructions  </a> 
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+            
+        </div>
+        <div id = "payment-mobile-view">
+                    <h1> Online Payments </h1>
+            <div id = "payment-header">
+                <img src="https://p2.piqsels.com/preview/271/703/795/copy-space-notebook-wooden-journal.jpg" alt=""></img>
+            </div>
+        
+            <div id = "payment-body">
+                <div id = "paypal-container">
+                    <div>
+                        <h2> Make a Payment </h2>
+                        <p> If you've used our services in the past, you can make a payment here! A 2% fee will be charged for all online/credit card payments </p>
+                        <PaypalButtons />
+                    </div>
+                </div>
+                
+                <div id = "donate-container">
+                    <div>
+                        <h2> Make a Donation </h2>
+                        <p> A tax-deductible donation to the non-profit, the <i> California Center for Cooperative Development </i> helps them support the sustainability 
+                            and growth of Co-op Homecare!</p>
+                            <div className ="link-container"><a href="https://cccd.coop/membership/donate-cccd"> Click to donate to CCCD  </a> </div>
+                    </div>
+                    
+                </div>
+
+                <div id = "check-container">
+                    <div>
+                        <h2> Pay by Check  </h2>
+                        <p> Payments via checks will not be charged a fee. Please send all checks  
+                            addressed to <strong> Co-op Home Care </strong>to the following address 
+                            <strong> 979 F street, Suite A-2, Davis, California 95616.</strong>.  
+                        </p>
+                        <div className ="link-container">
                             <a href="https://goo.gl/maps/ogjx6M2Ku7BCKEUA7"> Click for Instructions  </a> 
                         </div>
                     </div>
