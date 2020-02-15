22269b18c0e95fb2404d5bb65747538647211cba
Ruben Garcia <rubengarcia@Rubens-Air.hsd1.ca.comcast.net>
2020-02-15 08:34:08 -0800

    added mobile-friendly view for the contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Layout from '../components/layout'
 import '../styles/contact.css'
 import ContactTemplate from '../components/contactTemplate'
+import logo from "../images/coopLogobigger.jpg"
 
 const Contact = () => (
   <Layout>
@@ -11,7 +12,7 @@ const Contact = () => (
             <div>
                 <h1> Co-op Homecare </h1>
                 <div id = "location">
-                    <h1> Location: </h1>
+                    <h2> Location: </h2>
                     <div>
                         979 F Street, Suite A-1 <br/>
                         Davis, CA 95616<br/>
@@ -19,11 +20,14 @@ const Contact = () => (
                 </div>
                     
                 <div id = "direct-contact">
-                    <h1> Direct Contact: </h1>
+                    <h2> Direct Contact: </h2>
                     <div>
                         <a href="tel:530-297-1023"> Phone:  530-297-1032 </a> <br/>
                         <a href = "mailto: info@cccd.coop"> Email: info@cccd.coop</a> <br/>
                         Hours: 9am - 5pm <br/>
+                    </div>
+                    <div>
+                        <img className="logo" src= {logo} alt="the cooperative official logo"></img><br/>
                     </div>
                 </div>
             </div>
