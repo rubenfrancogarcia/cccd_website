58f63796d220bac7dccfa64fe706d5591922bacb
Ruben Garcia <rubengarcia@Rubens-Air.hsd1.ca.comcast.net>
2020-02-18 14:27:41 -0800

    refactor

--- src/pages/about.js
+++ src/pages/about.js
@@ -13,35 +13,48 @@ const About = () => (
             </div>
         
             <div id = "About-Body">
-                <div id = "About-Left"> 
-                    <h1> Co-op Homecare </h1>
-                    <p> 
-Thank you for considering Co-op Homecare. We specialize in providing quality, compassionate caregiving services.</p>
- 
-<p><b>Our mission is to provide compassionate, high quality in-home support services to our clients.</b></p>
- 
-<p>Our worker-owned cooperative offers a win-win for clients and caregivers. Caregivers co-own this business; we chose this work because we enjoy caregiving.  The worker cooperative model provides us control over our workplace policies and better compensation.  This benefits clients who gain quality services from a committed team.</p>  
- 
-<p><i>Co-op Homecare</i> was initiated as a project of the local non-profit, the California Center for Cooperative Development (CCCD).
-The premise of the project is that people who need home care support deserve quality services from a consistent caregiver, and 
-caregivers deserve work stability, livable wages, and positive working conditions. The worker cooperative model is mutually beneficial
-for clients and caregivers: caregivers gain workplace advantages that encourage them to stay, and clients receive quality, reliable care
-from a consistent caregiver.  CCCD expects to be providing assistance to the cooperative through 2021 to provide a solid foundation for 
-business success and growth. The goal is for the cooperative to grow and offer worker-cooperative opportunities to more caregivers within 
-Yolo County and to expand to provide quality and compassionate home care in Dixon, and, eventually, areas in Sacramento. 
-Donations to <a href="https://cccd.coop/">CCCD</a> support the non-profit’s support of this project.
- 
-                    </p>
-                    <a href="/services"> Learn more about what we do > </a>
-                </div>
+                   <div class= "content ">
+                        <div class="content-info ">
+                                    <h2> Our Mission</h2>
+                                    <div class="content mission ">
+                                    <p> 
+                                    Thank you for considering Co-op Homecare. We specialize in providing quality, compassionate caregiving services.</p>
+
+                                        <p><b>Our mission is to provide compassionate, high quality in-home support services to our clients.</b></p>
+                                    </div>
+                                    <h2> Our Philosophy</h2>
+                                    <div class="content philosophy">
+                                        <div>
+                                        <p>Our worker-owned cooperative offers a win-win for clients and caregivers. Caregivers co-own this business; we chose this work because we enjoy caregiving.  The worker cooperative model provides us control over our workplace policies and better compensation.  This benefits clients who gain quality services from a committed team.</p> 
+                                            </div> 
+                                            <div> 
+                                            <p><i>Co-op Homecare</i> was initiated as a project of the local non-profit, the California Center for Cooperative Development (CCCD).
+                                        The premise of the project is that people who need home care support deserve quality services from a consistent caregiver, and 
+                                        caregivers deserve work stability, livable wages, and positive working conditions. The worker cooperative model is mutually beneficial
+                                        for clients and caregivers: caregivers gain workplace advantages that encourage them to stay, and clients receive quality, reliable care
+                                        from a consistent caregiver.  </p>
+                                            </div>
+
+                            </div>
+                            <h2>Our Vision</h2>
+                            <div class="content vision">
+                            <p>CCCD expects to be providing assistance to the cooperative through 2021 to provide a solid foundation for 
+                            business success and growth. The goal is for the cooperative to grow and offer worker-cooperative opportunities to more caregivers within 
+                            Yolo County and to expand to provide quality and compassionate home care in Dixon, and, eventually, areas in Sacramento. </p>
+                            </div>
+                            <div class="donation">
+                            Donations to <a href="https://cccd.coop/">CCCD</a> support the non-profit’s support of this project. <a href="/services"> Learn more about what we do > </a>
+                            </div>
+                        </div>
+                        <div class="content-image image">
+                                <img src="https://i.imgur.com/pE5wX7L.jpg"  className="image" alt="Our Team!"/>
+                            </div>
+                   </div>
+
+               
+                    
+                    
                 
-                <div id = "About-Right">
-                    <div>
-                        <img src="https://i.imgur.com/pE5wX7L.jpg" alt="Our Team!"/>
-                    </div>
-                    
-                    
-                </div>
             </div>
             
         </div>
