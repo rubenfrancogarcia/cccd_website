cc7a28bef72626d381274e8cbe4b4ad353c08ac8
cccdCoop <59102697+cccdCoop@users.noreply.github.com>
2021-07-27 11:56:29 -0700

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -19,18 +19,17 @@ const About = () => (
                                     <h2 class={"content-header"}> Our Mission</h2>
                                     <div class="content mission ">
 
-                                    <p> 
-                                    Thank you for considering Co-op Homecare. We specialize in providing quality, compassionate caregiving services.</p>
-
-                                        <p><b>Our mission is to provide compassionate, high quality in-home support services to our clients.</b></p>
+                                 <p><b>Our mission is to provide compassionate, high quality in-home support services to our clients.</b></p>
                                     </div>
                                     <h2 class={"content-header"}> Our Philosophy</h2>
                                     <div class="content philosophy">
                                         <div>
-                                        <p>Our worker-owned cooperative offers a win-win for clients and caregivers. Caregivers co-own this business; we chose this work because we enjoy caregiving.  The worker cooperative model provides us control over our workplace policies and better compensation.  This benefits clients who gain quality services from a committed team.</p> 
+                                        <p>Our worker-owned cooperative offers a win-win for clients and caregivers. Our caregivers co-own the business and we chose this work because we enjoy caregiving.  
+                                        The worker-cooperative model gives us control over the decisions that affect our workplace and daily lives. This benefits our clients who gain quality services from a 
+                                        committed team.</p> 
                                             </div> 
                                             <div> 
-                                            <p><i>Co-op Homecare</i> was initiated as a project of the local non-profit, the California Center for Cooperative Development (CCCD).
+                                            <p><i>Co-op Home Care</i> was developed by a local non-profit, the California Center for Cooperative Development (CCCD).
                                         The premise of the project is that people who need home care support deserve quality services from a consistent caregiver, and 
                                         caregivers deserve work stability, livable wages, and positive working conditions. The worker cooperative model is mutually beneficial
                                         for clients and caregivers: caregivers gain workplace advantages that encourage them to stay, and clients receive quality, reliable care
@@ -41,17 +40,17 @@ const About = () => (
                             <h2 class={"content-header"} >Our Vision</h2>
                             <div class="content vision">
                               <p>
-                                Co-op Home Care will be the region’s number one source of In-home support serves. We intend to grow geographically,
-                                from providing care in Yolo County and Dixon to encompass regions in Sacramento. Growth will allow us to provide
+                                Co-op Home Care will be the region’s number one source of home care serves. As we grow geographically,
+                                we will providing care in Yolo County and Dixon as well as regions in Sacramento. Growth will allow us to provide
                                 services to more clients and to grow employment and co-ownership opportunities for more caregivers.
                               </p>
                               </div>
                               <div className="content vision">
-                            <p>CCCD will be providing assistance to the cooperative through 2021 to provide a solid foundation for
-                            business success and growth. The goal is for the cooperative to grow and offer worker-cooperative opportunities to more caregivers within the regions of care. </p>
+                            <p>The CCCD will be providing assistance to the cooperative through 2021 to develop a solid foundation for
+                            business success and growth. Our goal is to grow and offer more career opportunities to caregivers in the region. </p>
                             </div>
                             <div class="donation">
-                            Donations to <a href="https://cccd.coop/">CCCD</a> support the non-profit’s support of this project.
+                            Donations to the <a href="https://cccd.coop/">CCCD</a> will support the development of this project.
                             </div>
                         </div>
                         <div class="content-image image">
